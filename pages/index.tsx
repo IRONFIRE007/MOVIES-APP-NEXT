875cdbdb6dda35c9a0b4b4259901a1830c6ad9d0
Luis Agamez <Tankforceblack141@gmail.com>
2022-05-09 12:31:05 -0500

    Next UI Integrated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,10 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
 
 const HomePage: NextPage = () => {
   return (
     <div className="container">
-      <Head>
-        <title>Movies App</title>
-        <meta name="Mivoes App" content="Movies App description" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-             <h1>Hello World</h1>
+      <h1>Hello World</h1>
       </div>
   )
 }
